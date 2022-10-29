37ce7187baa1ed4f94d63fdba85a87c20cc8c055
ssatosays <ssatosays@gmail.com>
2022-10-29 22:21:11 +0900

    otameshi

--- tests/main.js
+++ tests/main.js
@@ -14,8 +14,12 @@ async function main(browser) {
   await page.goto('https://ssatosays.github.io/myage/');
   const e = await page.evaluate(() => {
     return {
-      title: document.title
+      title: document.title,
+      txt: document.querySelector('div #app div').textContent
     };
   });
   assert(e.title === 'myage');
+  console.log('--- debug');
+  console.log(e.txt);
+  console.log('--- debug');
 }
