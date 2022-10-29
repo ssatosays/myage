e26d5ae39c731c588d41b511c7c3e7810675c976
Sota Sato <86867075+ssatosays@users.noreply.github.com>
2022-10-29 22:38:46 +0900

    Add Test (#2)
    * install puppeteer
    
    * Create tests/
    
    * typo
    
    * Rename step name
    
    * otameshi
    
    * otameshi
    
    * Add asserts
    
    * Update daily.yml

---
+++ tests/main.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const puppeteer = require('puppeteer');
+
+puppeteer.launch(
+  {args: [], headless: true}
+).then(async browser => {
+  await main(browser);
+  browser.close();
+});
+
+async function main(browser) {
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('page console.log:', msg.text()));
+  await page.goto('https://ssatosays.github.io/myage/');
+  const e = await page.evaluate(() => {
+    return {
+      title: document.title,
+      txt: document.querySelector('#app div').textContent
+    };
+  });
+  assert(e.title === 'myage');
+  assert(e.txt === 'age: 28');
+}
