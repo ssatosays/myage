d96e248473c4c57db482a3fa5546d0b70b605a54
ssatosays <ssatosays@gmail.com>
2022-11-01 19:15:12 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="birthday">birthday: {{ birthday }}</div>
-  <div id="age">age: {{ age }}</div>
+  <div class="card">
+    <div id="birthday">birthday: {{ birthday }}</div>
+    <div id="today">today: {{ today }}</div>
+    <div id="age">age: {{ age }}</div>
+  </div>
 </template>
 
 <script>
@@ -10,20 +13,36 @@ export default {
   },
   data() {
     return {
-      birthday: '1993/11/10',
-      age: 0
+      birthday: '',
+      today: '',
+      age: 0,
+      internalData: {
+        birthday: {
+          year: 1993,
+          month: 11,
+          date: 10
+        }
+      }
     }
   },
   mounted() {
-    this.age = this.calc()
+    this.birthday = this.getBirthday()
+    this.today = this.getToday()
+    this.age = this.getAge()
   },
   methods: {
-    calc() {
-      const birthday = {
-        year: 1993,
-        month: 11,
-        date: 10
-      }
+    getBirthday() {
+      const birthday = this.internalData.birthday
+      return birthday.year + '/' + birthday.month + '/' + birthday.date
+    },
+    getToday() {
+      const today = new Date()
+      return today.getFullYear() + '/' + 
+        (today.getMonth() + 1).toString().padStart(2, "0") + '/' +
+        today.getDate().toString().padStart(2, "0")
+    },
+    getAge() {
+      const birthday = this.internalData.birthday
       const today = new Date()
       const thisYearBirthday = new Date(today.getFullYear(), birthday.month - 1, birthday.date)
       let age = today.getFullYear() - birthday.year
@@ -42,5 +61,17 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  font-size: 2rem;
+}
+div.card {
+  background: #f2f2f2;
+  border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #d9d9d9;
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding: 10px;
 }
 </style>
